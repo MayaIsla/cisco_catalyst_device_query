b769fff25b9492e4763081a294c461e72818a642
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2025-10-20 09:13:45 -0500

    added error handling, verifications of serial numbers and business object changes.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ parsed_auth_key_text = parsed_auth_key_json["Token"]
 catalyst_headers = {'X-Auth-Token': parsed_auth_key_text}
 device_auth_uri = 'https://server.local/dna/intent/api/v1/network-device'
 
-api_uri = "https://tenant.saasit.com/api/odata/businessobject/CIs"
+api_uri = "https://tenant.saasit.com/api/odata/businessobject/CI_ivnt_infrastructures" #Cannot use the CI business rule -_-
 iv_auth_header = {'Authorization': ivnt_api__Key}
 
 
@@ -30,10 +30,40 @@ for i in network_device_json.json()['response']:
     device_serial_number = i['serialNumber']
     device_platformId = i['platformId']
     device_managementIpAddress = i['managementIpAddress']
+    print(device_host_name + " " + device_serial_number + " " + device_platformId + " " + device_managementIpAddress)
     
-    ivnt_get_phone_exists = "https://tenant.saasit.com/api/odata/businessobject/CIs?$search=" + device_serial_number
     
-    if ivnt_get_phone_exists.status_code == 204: #204 no content - means switch doesn't exist
-      body_response = str('{"MACAddress": ' + '"' + device_host_name + '","IPAddress":' + '"' +  device_managementIpAddress + '","SerialNumber":' + '"' + device_serial_number + '"' +"}")
+    ivnt_get_phone_exists = "https://tenant.saasit.com/api/odata/businessobject/CI__ivnt_infrastructures?$search=" + device_serial_number
+    ivnt_get_phone = requests.get(url = ivnt_get_phone_exists, headers=iv_auth_header)
+    print(update_CI.text)
+    
+    if ivnt_get_phone.status_code == 204: #204 no content - means switch doesn't exist
+      body_response = str('{"IPAddress":' + '"' +  device_managementIpAddress + '","Status": "Production",' + '"ivnt_AssetSubtype": "Network Switch - 24p' +  '","CIType": "ivnt_Infrastructure' + '","SerialNumber":' + '"' + device_serial_number +  '","Name":' + '"' + device_host_name + '"' + "}")
+      # FullyQualifiedDomainName is "Name" + "DomainName" Setup as a business rule.
+      # Update body to add discovered data (Discovered manufacturer, model), Status, FQDN, IP, Mac Address (Verify with Catalyst API documentation).
       update_CI = requests.post(url= api_uri,  data= body_response, headers=iv_auth_header)
+      print(update_CI.text)
+        
+    else:
+      if ivnt_get_phone.status_code == 200: #Phone exists, update record with RecID.
+          request_iv_get_recID_text = ivnt_get_phone.text
+          json_RecID = json.loads(request_iv_get_recID_text)
+
+          for i in json_RecID['value']:
+              ivnt_ci_RecId = (i['RecId'])
+              
+          #ivnt_ci_mod_url = "https://becn-stg.saasit.com/api/odata/businessobject/CIs('" + ivnt_ci_RecId + "')"
+          ivnt_ci_mod_url = "https://tenant.saasit.com/api/odata/businessobject/CI__ivnt_infrastructures('" + ivnt_ci_RecId + "')"
+          body_response = str('{"IPAddress":' + '"' +  device_managementIpAddress + '","SerialNumber":' + '"' + device_serial_number + '","Name":' + '"' + device_host_name + '"' + "}")
+          update_existing_CI = requests.put(url=ivnt_ci_mod_url, data= body_response, headers=iv_auth_header)
+          print(update_existing_CI.text)
+          
+    else: 
+        print("No 200 or 204 error please look at transcript.") #error handles, log will show failed json.
+        #ivnt_get_phone = "https://becn-stg.saasit.com/api/odata/businessobject/CIs?$search=" + device_serial_number
+        ivnt_get_phone = "https://tenant.saasit.com/api/odata/businessobject/CI__ivnt_infrastructures?$search=" + device_serial_number
+        request_ivnt_phone_exists = requests.get(url = ivnt_get_phone, headers=iv_auth_header)
+        text_CSV =  request_ivnt_phone_exists.text
+        with open("C:/dir/to/error/logging/error_log.csv","w") as file:
+            file.write(text_CSV + "\n")
 
